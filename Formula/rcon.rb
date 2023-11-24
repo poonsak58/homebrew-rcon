903a0e6181475730ed5e5459be77846f19c4c889
Poonsak Aphidetmongkhon <poonsak_lds@hotmail.com>
2023-11-25 03:53:45 +0700

    Update rcon.rb

--- Formula/rcon.rb
+++ Formula/rcon.rb
@@ -1,8 +1,8 @@
 class Rcon < Formula
   desc "Source RCON client for shell"
   homepage "https://github.com/n0la/rcon"
-  url "https://github.com/n0la/rcon/archive/0.5.tar.gz"
-  sha256 "28155fca4de607e7ea7101985178aa7f29ea9de624760e4cbcd2746f7f7f6e65"
+  url "https://github.com/n0la/rcon/archive/0.6.tar.gz"
+  sha256 "c8d533319891534546dc23b8616aefadf8844e3f5f22674bb83fd69df1460540"
   license "BSD-2-Clause"
   head "https://github.com/n0la/rcon.git"
 
